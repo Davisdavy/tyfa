780be3a2ba5caea94a37ce2565bc03de7fec5596
Davisdavy <davisdavy96@gmail.com>
2021-04-19 12:41:22 +0300

    Custom input widget

--- lib/screen/register_page.dart
+++ lib/screen/register_page.dart
@@ -2,6 +2,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tyfa/constants.dart';
+import 'package:tyfa/widgets/custom_btn.dart';
 
 
 class RegisterPage extends StatefulWidget {
--- lib/widgets/custom_action_bar.dart
+++ lib/widgets/custom_action_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_app/services/firebase_services.dart';
 class CustomActionBar extends StatelessWidget {
   final String title;
   final bool hasBackArrrow;
